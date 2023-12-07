8046d21dea5bfcf321aa08a689aa841081327c2c
raffiMRG <mutekinoraffi@gmail.com>
2023-12-07 20:02:30 +0700

    second commit

---
+++ admin/footer.php
@@ -0,0 +1,14 @@
+<!-- Footer -->
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <!-- <span>© Copyright 2019 News Site | Powered by <a href="https://www.youtube.com/nirobhasan">Nirob Hasan</a></span> -->
+                <span>© Copyright 2023 News Site | Powered by <a >Toya</a></span>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /Footer -->
+</body>
+</html>
